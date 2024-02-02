4d2fa24c5ebbc4c09b1fea72592f3f876667bc0a
Gabriel Alzina Alomar <44143564+bielalzina@users.noreply.github.com>
2024-02-02 19:30:33 +0100

    Create sheets_demo.py

---
+++ sheets_demo.py
@@ -0,0 +1,7 @@
+import os
+from google import Create_Service
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
